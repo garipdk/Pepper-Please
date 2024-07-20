74b10356ced341f2614539c579c5f17533146197
ManonMarchand <manonmarchand22@gmail.com>
2024-07-20 19:35:33 +0200

    make bargain move

--- bargain/bargain.gd
+++ bargain/bargain.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var rng = RandomNumberGenerator.new()
-
+var multiplier: float = 1
 var bar_width: float
 var direction: int = 1
 var is_moving: bool = true
@@ -21,3 +21,15 @@ func _physics_process(delta):
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		is_moving = false
+		multiplier = calculate_bonus()
+		print(multiplier)
+		
+func calculate_bonus():
+	var final_position: float = $Arrow.position.x
+	if final_position > bar_width / 4:
+		return 0.95
+	if final_position > bar_width / 8:
+		return 1.05
+	if bar_width > bar_width / 16:
+		return 0.9	
+	return 1.1
