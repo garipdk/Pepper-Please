a03ceccfb25b1a2a706e6dc56b8e80193c583aba
Garip D. Kusoglu <garip.doga.kusoglu@gmail.com>
2024-07-20 21:53:56 +0200

    added intro text

--- map/map.gd
+++ map/map.gd
@@ -4,6 +4,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var file = "res://intro.json"
+	var json_as_text = FileAccess.get_file_as_string(file)
+	var json_as_dict:Dictionary = JSON.parse_string(json_as_text)
+	await  get_tree().process_frame
+	if json_as_dict.has("intro"):
+		$Label.text = json_as_dict.get("intro")
 	pass # Replace with function body.
 
 
@@ -24,4 +30,5 @@ func _on_map_place_mouse_control_gui_input(event:InputEvent, extra_arg_0:NodePat
 		$Path2D/PathFollow2D.progress_ratio = 0.
 		$Path2D/PathFollow2D.loop = false
 		$Path2D/PathFollow2D.is_moving = true
+		$Label.visible = false
 	pass # Replace with function body.
