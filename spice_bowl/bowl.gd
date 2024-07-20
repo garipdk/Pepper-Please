de05e1b1e3f23e31f8b55842847a4b730985fc4a
ManonMarchand <manonmarchand22@gmail.com>
2024-07-20 16:27:02 +0200

    add shelves in store

--- spice_bowl/bowl.gd
+++ spice_bowl/bowl.gd
@@ -6,35 +6,33 @@ signal no_more(spice_type: String)
 @export var min_value: float = 0
 @export var max_value: float = 1
 @export var spice_name: String = "salt"
-@export var init_ammount: int = 100
-
-var spice: PepperGlobal.Spice = PepperGlobal.Spice.new(spice_name, min_value, max_value, init_ammount)
+@export var ammount: int = 100
+@export var color: Color = Color(0, 0, 1, 0)
+var empty_bowl: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var bowl_position = $Sprite2D.get_rect().position
 	var bowl_width = $Sprite2D.texture.get_width() * $Sprite2D.scale[0]
+	%Line2D.default_color = color
 	%Line2D.set_point_position(0, Vector2(-bowl_width/2, 0))
-	%Line2D.set_point_position(1, Vector2(0, -spice.ammount))
+	%Line2D.set_point_position(1, Vector2(0, -ammount))
 	%Line2D.set_point_position(2, Vector2(bowl_width/2, 0))
 	
 	var store: Store = get_tree().get_nodes_in_group("store_group")[0]
 	store.transation_done.connect(_on_transaction)
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
-func _on_transaction(bargain_ammount: int, bargain_value:float, spice_type: String):
+func _on_transaction(bargain_ammount: int, spice_type: String):
 	print("transaction of" + spice_type)
-	if spice_type == spice.spice_name:
-		if bargain_ammount <= spice.ammount:
-			spice.ammount -= bargain_ammount * bargain_value
-			print(spice.ammount)
-			%Line2D.set_point_position(1, Vector2(0, -spice.ammount))
-		else:
-			no_more.emit(spice_type)
+	if spice_type == spice_name:
+		ammount -= bargain_ammount
+		print(ammount)
+		%Line2D.set_point_position(1, Vector2(0, -ammount))
+		if ammount == 0:
+			empty_bowl = true
 			
 	
