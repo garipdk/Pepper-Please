dd8ed76e5736476473d461a8a35f41f771883123
Garip D. Kusoglu <garip.doga.kusoglu@gmail.com>
2024-07-20 15:55:20 +0200

    add colision shape for places and import images

--- map/Place/PlaceCollisionShape2D.gd
+++ map/Place/PlaceCollisionShape2D.gd
@@ -1,8 +1,5 @@
 extends CollisionShape2D
 
-func _input(event):
-	if event is InputEventMouseMotion:
-		
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
