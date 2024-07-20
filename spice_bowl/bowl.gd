0a0eec88531122716482e99ca5bd5e2f6529f3c4
ManonMarchand <manonmarchand22@gmail.com>
2024-07-20 08:45:05 +0200

    feat: spices can now have variable price

--- spice_bowl/bowl.gd
+++ spice_bowl/bowl.gd
@@ -3,19 +3,19 @@ extends Node2D
 
 signal no_more(spice_type: String)
 
-@export
-var spice_name: String = "salt"
-@export
-var spice_ammount: int = 100
-@export
-var spice_value: float = 1.
+@export var min_value: float = 0
+@export var max_value: float = 1
+@export var spice_name: String = "salt"
+@export var init_ammount: int = 100
+
+var spice: PepperGlobal.Spice = PepperGlobal.Spice.new(spice_name, min_value, max_value, init_ammount)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var bowl_position = $Sprite2D.get_rect().position
 	var bowl_width = $Sprite2D.texture.get_width() * $Sprite2D.scale[0]
 	%Line2D.set_point_position(0, Vector2(-bowl_width/2, 0))
-	%Line2D.set_point_position(1, Vector2(0, -spice_ammount))
+	%Line2D.set_point_position(1, Vector2(0, -spice.ammount))
 	%Line2D.set_point_position(2, Vector2(bowl_width/2, 0))
 	
 	var store: Store = get_tree().get_nodes_in_group("store_group")[0]
@@ -27,12 +27,13 @@ func _ready():
 func _process(delta):
 	pass
 	
-func _on_transaction(ammount: int, spice_type: String):
+func _on_transaction(bargain_ammount: int, bargain_value:float, spice_type: String):
 	print("transaction of" + spice_type)
-	if spice_type == spice_name:
-		if ammount * spice_value <= spice_ammount:
-			spice_ammount -= ammount * spice_value
-			%Line2D.set_point_position(1, Vector2(0, -spice_ammount))
+	if spice_type == spice.spice_name:
+		if bargain_ammount <= spice.ammount:
+			spice.ammount -= bargain_ammount * bargain_value
+			print(spice.ammount)
+			%Line2D.set_point_position(1, Vector2(0, -spice.ammount))
 		else:
 			no_more.emit(spice_type)
 			
--- store/store.gd
+++ store/store.gd
@@ -1,8 +1,16 @@
 class_name Store
 extends Node2D
 
+var list_spices: Dictionary = {
+	SALT = "salt",
+	LAUREL = "laurel",
+	CINNAMON = "cinnamon",
+	FENUGREEK = "fenugreek",
+	SAFFRON = "saffron"
+}
+
 var money: int = 0
-signal transation_done(int)
+signal transation_done(int, float, String)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,10 +21,10 @@ func _ready():
 func _process(delta):
 	pass
 
-func do_transaction(amount: int, spice_type: String):
+func do_transaction(amount: int, bargain_value:float, spice_type: String):
 	money += amount
 	$Label.text = str(money)
-	transation_done.emit(amount, spice_type)
+	transation_done.emit(amount, bargain_value, spice_type)
 
 func _on_button_pressed():
-	do_transaction(10, "salt")
+	do_transaction(10, 1, "salt")
