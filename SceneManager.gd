a987e1c70aff4ff8f7b3c8d835a8e6bab2c37655
Garip D. Kusoglu <garip.doga.kusoglu@gmail.com>
2024-07-20 20:27:39 +0200

    added quit button

--- SceneManager.gd
+++ SceneManager.gd
@@ -1,17 +1,16 @@
 extends Node2D
 
 var current_destination_map:NodePath
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	await get_tree().process_frame
 	$Shelves.connect_all_transations($Store)
+	$TextureRect.scale = Vector2(0.5, 0.5)
 	pass # Replace with function body.
 
 func _input(event:InputEvent):
 	if event.is_action_pressed("ui_cancel"):
 		move_child($MapScene, 1)
-		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -32,4 +31,10 @@ func _on_shelves_ready():
 
 func _on_area_2d_body_entered(_body, extra_arg_0):
 	current_destination_map = extra_arg_0
+	pass # Replace with function body.
+
+
+func _on_texture_rect_gui_input(event:InputEvent):
+	if event is InputEventMouseButton and event.pressed:
+		get_tree().quit()
 	pass # Replace with function body.
