965dd7a4509f03d53a9642ddee5a028240bc0b4d
Garip D. Kusoglu <garip.doga.kusoglu@gmail.com>
2024-07-21 11:46:15 +0200

    menu button is now not visible on the menu

--- SceneManager.gd
+++ SceneManager.gd
@@ -10,6 +10,7 @@ func _ready():
 func _input(event:InputEvent):
 	if event.is_action_pressed("ui_cancel"):
 		move_child($MapScene, 1)
+		$TextureButton.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -19,6 +20,7 @@ func _process(_delta):
 		if current_destination_map.get_name(0) == "Store":
 			move_child(get_node(current_destination_map), 1)
 		PepperGlobal.canChangeScene = false
+		$TextureButton.visible = true
 
 
 func _on_shelves_ready():
@@ -36,3 +38,4 @@ func _on_texture_rect_gui_input(event:InputEvent):
 
 func _on_texture_button_pressed():
 	move_child($MapScene, 1)
+	$TextureButton.visible = false
