bee34fb80d690f9d4a8055010d5d3e3bac02e4da
ManonMarchand <manonmarchand22@gmail.com>
2024-07-21 14:47:44 +0200

    port mechanics working

--- port/port.gd
+++ port/port.gd
@@ -7,14 +7,31 @@ var spices_colors: Array[Color] = [
 	Color("#cc1d1d")
 ]
 
+var spices_names = [
+	"laurel", "cinnamon", "fenugreek", "saffron"
+]
+
+var spices_price = [
+	500, 1000, 5000, 10000
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	await  get_tree().process_frame
-	$PanelContainer/Label.text = PepperGlobal.dialogs.get("boat").get("first_visit")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	update_text()
+	
+func update_text():
+	$PanelContainer/RichTextLabel.text = ""
+	$PanelContainer/RichTextLabel.text = PepperGlobal.dialogs.get("boat").get("first_visit")
+	$PanelContainer/RichTextLabel.add_text("Today I found some ")
+	$PanelContainer/RichTextLabel.push_color(spices_colors[PepperGlobal.number_of_unlocked_spices])
+	$PanelContainer/RichTextLabel.add_text(" {next_spice}".format({"next_spice": spices_names[PepperGlobal.number_of_unlocked_spices]}))
+	$PanelContainer/RichTextLabel.push_color("white")
+	$PanelContainer/RichTextLabel.add_text(". I can sell that to yerr for {price} gold nuggets.".format({"price": spices_price[PepperGlobal.number_of_unlocked_spices]}))
+	$SpicePile.modulate = spices_colors[PepperGlobal.number_of_unlocked_spices]
 
 func _on_texture_button_pressed():
-	if money > 
+	if PepperGlobal.money >= spices_price[PepperGlobal.number_of_unlocked_spices]:
+		PepperGlobal.number_of_unlocked_spices += 1
+		update_text()
+		
