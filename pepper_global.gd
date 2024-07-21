c36b36e97e5e082234d0ff53427b9314d92a635f
ManonMarchand <manonmarchand22@gmail.com>
2024-07-21 12:38:43 +0200

    add dialogs to pepper global

--- pepper_global.gd
+++ pepper_global.gd
@@ -3,3 +3,6 @@ extends Node
 var shelves:Node2D
 
 var canChangeScene:bool = false
+
+var json_as_text = FileAccess.get_file_as_string("res://intro.json")
+var dialogs:Dictionary = JSON.parse_string(json_as_text)
