021bf7815d7b375bf2cc595f9f7ab89d2f8e28e0
ManonMarchand <manonmarchand22@gmail.com>
2024-07-21 10:54:41 +0200

    bargain finally works

--- bargain/bargain.gd
+++ bargain/bargain.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal bargain_done(multiplier: float)
+
 var rng = RandomNumberGenerator.new()
 var multiplier: float = 1
 var bar_width: float
@@ -10,30 +12,25 @@ func _ready():
 	await get_tree().process_frame
 	bar_width = $Sprite2D.texture.get_width() * $Sprite2D.scale[0]
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if is_moving == true:
-		$Arrow.position.x += direction*10*delta
+		$Arrow.position.x += direction*20*delta
 		if absf($Arrow.position.x) >= bar_width / 2:
 			direction *= -1
-			
-func _input(event: InputEvent) -> void:
-
-	if event is InputEventMouseButton:
-		var mousePos = get_global_mouse_position()
-		# Check if there is a collision at the mouse position
-		#if $Sprite2D/Area2D/CollisionShape2D.intersect_point(mousePos, 1):
-		is_moving = false
-		multiplier = calculate_bonus()
-		print(multiplier)
 		
 func calculate_bonus():
-	var final_position: float = $Arrow.position.x
+	var final_position: float = absf($Arrow.position.x)
 	if final_position > bar_width / 4:
 		return 0.95
 	if final_position > bar_width / 8:
 		return 1.05
-	if bar_width > bar_width / 16:
+	if final_position > bar_width / 16:
 		return 0.9
 	return 1.1
+
+func _on_bargain_button_pressed():
+	is_moving = false
+	$Sprite2D/BargainButton.disabled = true
+	multiplier = calculate_bonus()
+	bargain_done.emit(multiplier)
--- store/store.gd
+++ store/store.gd
@@ -7,6 +7,11 @@ var money: int = 50
 signal transation_done(int, String)
 var transaction: Dictionary
 var list_spices: Array[SpiceBowl]
+var multiplier: float = 1
+
+var bargain_value: int
+var new_value: int
+var ammount: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,21 +19,24 @@ func _ready():
 	list_spices = [PepperGlobal.shelves.get_node("Salt"), PepperGlobal.shelves.get_node("Laurel"),
 	PepperGlobal.shelves.get_node("Cinnamon"), PepperGlobal.shelves.get_node("Fenugreek"), PepperGlobal.shelves.get_node("Saffron")]	
 	transaction = new_transaction(list_spices)
+	$BargainMinigame/Sprite2D/BargainButton.disabled = true
+	$BargainMinigame.bargain_done.connect(_on_bargain_done)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 	
 func new_transaction(list_spices):
+
 	if are_all_bowls_empty():
 		$PanelContainer/Dialog.text = "Ow oh! \n There's no more spices to sell here. Maybe I should explore a bit around?"
 		$Accept.visible = false
 		$Bargain.visible = false
 		return {}
+	$Bargain.visible = true
+	$Accept.visible = true
+	multiplier = 1
 	var rand_spice_number: int = rng.randi_range(0, number_of_unlocked_spices)
 	var spice_bowl: SpiceBowl = list_spices[rand_spice_number]
-	var ammount: int = rng.randi_range(1, spice_bowl.ammount)
-	var bargain_value: int = rng.randi_range(spice_bowl.min_value, spice_bowl.max_value)
+	ammount = rng.randi_range(1, spice_bowl.ammount)
+	bargain_value = rng.randi_range(spice_bowl.min_value, spice_bowl.max_value)
 	# TODO: add png in text 
 	$PanelContainer/Dialog.text = "Hey, I'd like to buy {ammount}g of ".format({"ammount": ammount})
 	$PanelContainer/Dialog.push_color(spice_bowl.color)
@@ -37,7 +45,7 @@ func new_transaction(list_spices):
 	$PanelContainer/Dialog.add_text("for {value} gold nuggets.".format({"value": ammount*bargain_value}))
 	return {"spice_name": spice_bowl.spice_name,
 			"ammount_sold": ammount,
-			"gold_gained": ammount * bargain_value
+			"gold_gained": roundi(ammount * bargain_value * multiplier)
 			}
 	
 func do_transaction(transaction: Dictionary):
@@ -58,4 +66,21 @@ func are_all_bowls_empty():
 		return true
 
 func _on_bargain_pressed():
+	$PanelContainer/Dialog.add_text("You don't think this is a fair price? Let me inspect the spices a bit more...")
+	$Bargain.visible = false
+	$Accept.visible = false
 	$BargainMinigame.visible = true
+	$BargainMinigame.is_moving = true
+	$BargainMinigame/Sprite2D/BargainButton.disabled = false
+	
+func _on_bargain_done(bargain_multiplier):
+	$Accept.visible = true
+	multiplier = bargain_multiplier
+	new_value = roundi(ammount * bargain_value * multiplier)
+	if multiplier > 1:
+		$PanelContainer/Dialog.add_text("Arf, sure... You're right. This sould be {new_value} golds.".format({"new_value":new_value}))
+	elif multiplier <= 1:
+		$PanelContainer/Dialog.add_text("Ahah nope! This looks so bland. I won't give more than {new_value} golds.".format({"new_value":new_value}))
+	$BargainMinigame.visible = false
+	
+	
